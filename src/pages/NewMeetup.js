74cd7285e89c1d70934eb600d7669453b9b97eaa
Amir Raouf <amir.raouf@192.168.1.2>
2021-05-31 00:43:24 +0200

    Fetch and create

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,21 +1,28 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup(props) {
-    function addMeetup(meetup) {
-        fetch('https://react-test-bf066-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
-        {
-            method: 'POST',
-            body: JSON.stringify(meetup),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
-    return (
-        <div>
-            <NewMeetupForm newMeetup={addMeetup}></NewMeetupForm>
-        </div>
-    )
+  const history = useHistory();
+
+  function addMeetup(meetup) {
+    fetch(
+      "https://react-test-bf066-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetup),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <div>
+      <NewMeetupForm newMeetup={addMeetup}></NewMeetupForm>
+    </div>
+  );
 }
 
 export default NewMeetup;
