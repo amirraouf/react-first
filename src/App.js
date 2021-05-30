59b9bd5d7cb61bbeaf828579ea0c4b8400601760
Amir Raouf <amir.raouf@192.168.1.2>
2021-05-30 20:49:45 +0200

    init..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Backdrop from "./components/Backdrop";
-import Modal from "./components/Modal";
-import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
-      <h1>My todos!</h1>
-      <Todo title='Learn React' />
-      <Todo title='Master React' />
+      
     </div>
   );
 }
